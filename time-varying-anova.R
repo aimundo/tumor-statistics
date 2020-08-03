d434c73046dcd9404b984854b328137530a2f359
jtipton25 <jtipton25@users.noreply.github.com>
2020-08-03 10:07:32 -0500

    updated script. Tried to fit with pre-built model rather than custom model but plotting results is more difficult.

--- time-varying-anova.R
+++ time-varying-anova.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(splines)
+library(rstan)
+library(rstanarm)
 
 set.seed(11)
 n_time      <- 10
@@ -80,21 +82,8 @@ dat_fit <- list(
 )
 
 fit <- stan(file = here::here("time-varying.stan"), data = dat_fit)
+e <- rstan::extract(fit)
 
-
-
-
-
-
-
-
-
-
-
-
-library(rstanarm)
-fit <- stan_gamm4(y ~ s(time, treatment), data = dat)
-e <- extract(fit)
 mu_post <- sapply(1:nrow(e$sigma), function(i) X_bs %*% e$beta[i, , ],
                   simplify = "array")
 dimnames(mu_post) <- list(
@@ -136,3 +125,139 @@ dat_plot %>%
 
 
 
+
+
+
+
+## Using pre-built software
+## need to figure out the plotting better but it should work too.
+
+
+# library(rstanarm)
+# library(tidybayes)
+# fit <- stan_gamm4(y ~ s(time, by = treatment), data = dat)
+# get_variables(fit)
+#
+# plot_nonlinear(fit)
+#
+#
+# XZ <- fit$x
+#
+# B <- as.matrix(fit)[, colnames(XZ), drop = FALSE]
+# labels <- sapply(fit$jam$smooth, "[[", "label")
+# xnames <- sapply(fit$jam$smooth, "[[", "vn")
+# names(fit$jam$smooth) <- labels
+# names(xnames) <- labels
+# fs <- sapply(fit$jam$smooth, FUN = "inherits", what = "fs.interaction")
+# smooths <- 1:length(labels)
+# original <- fit$jam$model
+#
+#
+#
+#
+# prob = 0.9
+# scheme <- bayesplot::color_scheme_get()
+# size = 0.75
+# alpha = 1
+# facet_args <- list()
+#
+# df_list <- lapply(fit$jam$smooth[smooths], FUN = function(s) {
+#     incl <- s$first.para:s$last.para
+#     b <- B[, incl, drop = FALSE]
+#     if (inherits(s, "fs.interaction")) {
+#         xx <- original[, s$base$term]
+#         fac <- original[, s$fterm]
+#         out <- by(data.frame(fac, xx), list(fac), FUN = function(df) {
+#             df <- df[order(df[, 2]), ]
+#             names(df) <- c(s$fterm, s$base$term)
+#             xz <- mgcv::PredictMat(s, df)
+#             f <- linear_predictor.matrix(b, xz)
+#             data.frame(predictor = df[, 2], lower = apply(f,
+#                                                           2, quantile, probs = (1 - prob)/2), upper = apply(f,
+#                                                                                                             2, quantile, probs = prob + (1 - prob)/2),
+#                        middle = apply(f, 2, median), term = paste(s$label,
+#                                                                   df[, 1], sep = "."))
+#         })
+#         do.call(rbind, args = out)
+#     }
+#     else {
+#         xz <- XZ[, incl, drop = FALSE]
+#         x <- original[, s$term]
+#         ord <- order(x)
+#         x <- x[ord]
+#         xz <- xz[ord, , drop = FALSE]
+#         if (!is.null(s$by.level)) {
+#             fac <- original[, s$by][ord]
+#             mark <- fac == s$by.level
+#             x <- x[mark]
+#             xz <- xz[mark, , drop = FALSE]
+#         }
+#         f <- rstanarm:::linear_predictor.matrix(b, xz)
+#         data.frame(predictor = x, lower = apply(f, 2, quantile,
+#                                                 probs = (1 - prob)/2), upper = apply(f, 2, quantile,
+#                                                                                      probs = prob + (1 - prob)/2), middle = apply(f,
+#                                                                                                                                   2, median), term = s$label)
+#     }
+# })
+#
+#
+#
+#
+# plot_data <- do.call(rbind, df_list)
+# str(plot_data)
+# facet_args[["facets"]] <- ~term
+#
+# ## recode the factors
+# levels(plot_data$term) <- c("1", "2", "3")
+#
+# dat_full <- plot_data %>%
+#     rename(time = predictor,
+#            mean = middle,
+#            treatment = term) %>%
+#     merge(
+#         data.frame(
+#             time      = rep(1:n_time, times = n_treatment),
+#             treatment = factor(rep(1:n_treatment, each = n_time)),
+#             truth     = c(mu)
+#         )
+#     )
+#
+# fit %>%
+#     spread_draws(Intercept)
+#
+# ## looks like there needs to be the intercept term
+#
+# dat_full %>%
+#     ggplot(aes(x = time, y = truth, color = treatment, group = treatment)) +
+#     geom_line(lwd = 1.5) +
+#     # geom_line(aes(x = time, y = mean, color = treatment, group = treatment)) +
+#     scale_color_viridis_d(end = 0.75) +
+#     scale_fill_viridis_d(end = 0.75) +
+#     ggtitle("Fitted time-varying responses") +
+#     theme_bw() +
+#     geom_ribbon(aes(ymin = lower, ymax = upper, fill = treatment), alpha = 0.5, color = NA) +
+#     # geom_ribbon(aes(ymin = lower_95, ymax = upper_95, fill = treatment), alpha = 0.25, color = NA) +
+#     geom_point(data = dat, aes(x = time, y = y))
+#
+#
+#
+#
+#
+# ggplot
+# # %>%
+#     merge(dat_full, dat_truth)
+#
+# ggplot(plot_data, aes_(x = ~predictor)) +
+#     geom_ribbon(aes_(ymin = ~lower,
+#                      ymax = ~upper), fill = scheme[[1]], color = scheme[[2]],
+#                 alpha = alpha, size = size) +
+#     geom_line(aes_(y = ~middle),
+#               color = scheme[[5]], size = 0.75 * size, lineend = "round") +
+#     labs(y = NULL) + do.call(facet_wrap, facet_args) + bayesplot::theme_default()
+#
+#
+#
+#
+#
+#
+#
