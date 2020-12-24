dbb9667ea00501795d6dd5dd4c32661067e054b0
aimundo <aimundo@uark.edu>
2020-12-24 16:18:23 -0600

    Trying to plot smooths over data

--- splines-missing-data.R
+++ splines-missing-data.R
@@ -69,6 +69,46 @@ layout(matrix(1:4, 2, 2))
 plot(mod1, main = "Missing")
 plot(mod2, main = "Full")
 
+par(mfrow=c(1,1))
 
 gam.check(mod1)
 gam.check(mod2)
+
+#plotting smooths and simulated data
+#full data
+full_predict<-with(dat_sim,expand.grid(observation=seq(min(observation),
+                                                       max(observation),length=400),
+                                                    Group=Group,Day=Day))
+
+full_predict<-cbind(full_predict,
+                         predict(mod2,full_predict,
+                                 se.fit = TRUE,type='response'))
+
+
+ggplot(data=full_predict,aes(x=Day,y=fit))+geom_point()+facet_wrap(~Group)
+
+ggplot(data=dat_sim, aes(x=Day, y=observation, group=Group)) +
+    facet_wrap(~Group) +
+    geom_point(colour='black',size=1,alpha=0.5)+
+    geom_ribbon(aes(ymin=(fit - 2*se.fit), ymax=(fit + 2*se.fit), x=Day),
+                data=full_predict, alpha=0.3,
+                inherit.aes=FALSE) +
+    geom_line(aes(x=Day,y=(fit),color=factor(Group)), size=1,data=full_predict,show.legend = FALSE)
+
+#missing data
+missing_predict<-with(dat_missing,expand.grid(observation=seq(min(observation),
+                                                       max(observation),length=400),
+                                       Group=levels(Group),Day=Day))
+
+missing_predict<-cbind(missing_predict,
+                    predict(mod1,missing_predict,
+                            se.fit = TRUE,type='response'))
+
+
+ggplot(data=dat_missing, aes(x=Day, y=observation, group=Group)) +
+    facet_wrap(~Group) +
+    geom_point(colour='black',size=1,alpha=0.5)+
+    geom_ribbon(aes(ymin=(fit - 2*se.fit), ymax=(fit + 2*se.fit), x=Day),
+                data=missing_predict, alpha=0.3,
+                inherit.aes=FALSE) +
+    geom_line(aes(y=(fit),color=factor(Group)), size=1,data=missing_predict,show.legend = FALSE)
